051f8a138efef2d77fe919ac37b9216fd9e645d1
Taniapalit <88310174+Taniapalit@users.noreply.github.com>
2021-08-02 16:54:50 +0530

    Update main.go

--- quickstart-automate/main.go
+++ quickstart-automate/main.go
@@ -18,31 +18,16 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
+
+	"github.com/gorilla/mux"
 )
 
-func main() {
-	log.Print("starting server...")
-	http.HandleFunc("/", handler)
-
-	// Determine port for HTTP service.
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-		log.Printf("defaulting to port %s", port)
-	}
-
-	// Start HTTP server.
-	log.Printf("listening on port %s", port)
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		log.Fatal(err)
-	}
+func homeLink(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome home!")
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	name := os.Getenv("NAME")
-	if name == "" {
-		name = "World"
-	}
-	fmt.Fprintf(w, "Hello %s!\n", name)
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", homeLink)
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
