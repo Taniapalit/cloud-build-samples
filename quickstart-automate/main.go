fa706e44b4743fc1ad60644eac1c7527b1fc73d8
Taniapalit <88310174+Taniapalit@users.noreply.github.com>
2021-08-03 13:37:39 +0530

    Update main.go

--- quickstart-automate/main.go
+++ quickstart-automate/main.go
@@ -14,8 +14,15 @@
 
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/marcosap/go-rest/internal/api"
+)
 
 func main() {
 	log.Println("I'm go-rest, nice to meet you!")
+
+	api := api.NewAPI()
+	api.Start()
 }
